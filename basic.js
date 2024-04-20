f2ed9ba8b327cfeb4f813fbd42a356014e022c3c
Saheb Kumar Ghale <140256595+SKGHALE@users.noreply.github.com>
2024-04-20 17:16:50 +0530

    Add files via upload

---
+++ basic.js
@@ -0,0 +1,14 @@
+const accountId = 324262
+let accountEmail="sahebkumarghale@gmail.com"
+var  accountPassword = "12344"
+
+console.log(accountId)
+
+ //accountEmail="ghalesahebkumar"
+console.log(accountEmail)
+
+// console.table([accountId,accountEmail,accountPassword])
+let accountState
+accountEmail="ghalesahebkumar"
+accountPassword = "23455"
+console.table([accountId,accountEmail,accountPassword,accountState])
